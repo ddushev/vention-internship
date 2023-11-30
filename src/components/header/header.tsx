e12f3f894c94bd5963e85aa571cfa1edc6772901
Daniel Dushev <danieldyshew@gmail.com>
2023-11-30 12:06:53 +0200

    Styles. Refactor header to use module css

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,29 +1,29 @@
-import "./header.scss";
-
 // eslint-disable-next-line import/no-extraneous-dependencies
 import { NavLink } from "react-router-dom";
 
 import PATHS from "@/utils/paths";
 
+import styles from "./header.module.scss";
+
 export default function Header() {
   return (
-    <header className="header-container">
+    <header className={styles.headerContainer}>
       <h2>Games Market</h2>
       <nav>
-        <ul className="navbar-link-items">
-          <li className="link-item">
+        <ul className={styles.navbarLinkItems}>
+          <li className={styles.linkItem}>
             <NavLink to={PATHS.HOME}>Home</NavLink>
           </li>
-          <li className="link-item">
+          <li className={styles.linkItem}>
             <NavLink to={PATHS.PRODUCTS}>Products</NavLink>
           </li>
-          <li className="link-item">
+          <li className={styles.linkItem}>
             <NavLink to={PATHS.ABOUT}>About</NavLink>
           </li>
-          <li className="link-item">
+          <li className={styles.linkItem}>
             <NavLink to={PATHS.SIGN_IN}>Sign In</NavLink>
           </li>
-          <li className="link-item">
+          <li className={styles.linkItem}>
             <NavLink to={PATHS.SIGN_UP}>Sign Up</NavLink>
           </li>
         </ul>
