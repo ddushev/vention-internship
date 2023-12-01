562097b929c8b0dd5ff9da55f35580dc59eec60d
Daniel Dushev <danieldyshew@gmail.com>
2023-12-01 18:36:00 +0200

    Component. Add error boundary wrapper in app component and default layout wrapper and temp path for error simulation

--- src/app.tsx
+++ src/app.tsx
@@ -5,20 +5,79 @@ import PATHS from "./utils/paths";
 import Header from "./components/header/header";
 import Page from "./elements/page";
 import Footer from "./components/footer";
+import ErrorBoundary from "./components/error/errorBoundary/errorBoundary";
+import SimulateError from "./components/simulateError";
 
-export default function App() {
+// Define a layout component for routes with Header and Footer
+function DefaultLayout({ children }: { children: React.ReactNode }) {
   return (
     <>
       <Header />
-      <Routes>
-        <Route path={PATHS.HOME} element={<Page title="Home page" />} />
-        <Route path={PATHS.ABOUT} element={<Page title="About page" />} />
-        <Route path={PATHS.PRODUCTS} element={<Page title="Products page" />} />
-        <Route path={PATHS.SIGN_IN} element={<Page title="Sign In page" />} />
-        <Route path={PATHS.SIGN_UP} element={<Page title="Sign Up page" />} />
-        <Route path="*" element={<Page title="Home page" />} />
-      </Routes>
+      {children}
       <Footer />
     </>
+  );
+}
+
+export default function App() {
+  return (
+    <ErrorBoundary>
+      <Routes>
+        {/* Default layout for most routes */}
+        <Route
+          path={PATHS.HOME}
+          element={
+            <DefaultLayout>
+              <Page title="Home page" />
+            </DefaultLayout>
+          }
+        />
+        <Route
+          path={PATHS.ABOUT}
+          element={
+            <DefaultLayout>
+              <Page title="About page" />
+            </DefaultLayout>
+          }
+        />
+        <Route
+          path={PATHS.PRODUCTS}
+          element={
+            <DefaultLayout>
+              <Page title="Products page" />
+            </DefaultLayout>
+          }
+        />
+        <Route
+          path={PATHS.SIGN_IN}
+          element={
+            <DefaultLayout>
+              <Page title="Sign In page" />
+            </DefaultLayout>
+          }
+        />
+        <Route
+          path={PATHS.SIGN_UP}
+          element={
+            <DefaultLayout>
+              <Page title="Sign Up page" />
+            </DefaultLayout>
+          }
+        />
+
+        {/* Temporary component and route for testing with simulated error*/}
+        <Route path="/component-with-error" element={<SimulateError />} />
+
+        {/* Fallback route */}
+        <Route
+          path="*"
+          element={
+            <DefaultLayout>
+              <Page title="Home page" />
+            </DefaultLayout>
+          }
+        />
+      </Routes>
+    </ErrorBoundary>
   );
 }
