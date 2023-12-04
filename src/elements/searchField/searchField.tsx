26df3143ef2aba229dae4c891f31c0b357a2d98e
Daniel Dushev <danieldyshew@gmail.com>
2023-12-04 18:33:20 +0200

    searchField. add loading animation and some hardcoded data

--- src/elements/searchField/searchField.tsx
+++ src/elements/searchField/searchField.tsx
@@ -1,12 +1,26 @@
+import loadingAnimation from "images/loading.svg";
+
 import { useState } from "react";
 import styles from "./searchField.module.css";
 
 export default function SearchField() {
   const [searchTerm, setSearchTerm] = useState("");
+  const [isLoading] = useState(false);
+  const tempData = ["Game1", "Game2", "Game3", "Game4", "Game5"];
 
   return (
     <div className={styles.searchField}>
       <input value={searchTerm} onChange={(e) => setSearchTerm(e.target.value)} type="text" placeholder="Search" />
+      {isLoading && (
+        <div className={styles.loader}>
+          <img src={loadingAnimation} alt="loading" />
+        </div>
+      )}
+      <ul>
+        {tempData.map((result) => (
+          <li>{result}</li>
+        ))}
+      </ul>
     </div>
   );
 }
