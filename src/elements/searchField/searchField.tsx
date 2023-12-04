8bc063a1faac8ff63f29a52f1508485f1deb9a81
Daniel Dushev <danieldyshew@gmail.com>
2023-12-04 21:47:45 +0200

    searchField. add conditional rendering for search results

--- src/elements/searchField/searchField.tsx
+++ src/elements/searchField/searchField.tsx
@@ -1,6 +1,6 @@
 import loadingAnimation from "images/loading.svg";
 
-import { useState } from "react";
+import { ChangeEvent, useState } from "react";
 import styles from "./searchField.module.css";
 
 export default function SearchField() {
@@ -8,19 +8,25 @@ export default function SearchField() {
   const [isLoading] = useState(false);
   const tempData = ["Game1", "Game2", "Game3", "Game4", "Game5"];
 
+  function handleInputChange(event: ChangeEvent<HTMLInputElement>) {
+    setSearchTerm(event.target.value);
+  }
+
   return (
     <div className={styles.searchField}>
-      <input value={searchTerm} onChange={(e) => setSearchTerm(e.target.value)} type="text" placeholder="Search" />
+      <input value={searchTerm} onChange={handleInputChange} type="text" placeholder="Search" />
       {isLoading && (
         <div className={styles.loader}>
           <img src={loadingAnimation} alt="loading" />
         </div>
       )}
-      <ul>
-        {tempData.map((result) => (
-          <li>{result}</li>
-        ))}
-      </ul>
+      {tempData.length > 0 && (
+        <ul className={styles.resultsList}>
+          {tempData.map((result) => (
+            <li key={result}>{result}</li>
+          ))}
+        </ul>
+      )}
     </div>
   );
 }
