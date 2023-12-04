c6a463fce2b8b1330eb7d22608f9a577c99f55a4
Daniel Dushev <danieldyshew@gmail.com>
2023-12-04 22:16:00 +0200

    api. add search endpoint and mock requests with mock data

--- src/api.mock.ts
+++ src/api.mock.ts
@@ -10,6 +10,22 @@ const mockData = {
   email: "willsmith321@gmail.com",
 };
 
-export default webpackMockServer.add((app) => {
+export default webpackMockServer.add((app, helper) => {
   app.get(apiEndpoints.testMock, (_req, res) => res.json(mockData));
+
+  app.get(apiEndpoints.searchMock, (_req, res) => {
+    const searchMockData = [
+      { id: helper.getUniqueIdInt(), name: "Battlefield" },
+      { id: helper.getUniqueIdInt(), name: "Counter-Strike" },
+      { id: helper.getUniqueIdInt(), name: "Genshin Impact" },
+      { id: helper.getUniqueIdInt(), name: "GTA" },
+      { id: helper.getUniqueIdInt(), name: "Minecraft" },
+      { id: helper.getUniqueIdInt(), name: "Overwatch" },
+      { id: helper.getUniqueIdInt(), name: "Sims 4" },
+      { id: helper.getUniqueIdInt(), name: "Terraria" },
+    ];
+    const searchText = _req.params.text.toLowerCase();
+    const matchingProducts = searchMockData.filter((game) => game.name.toLowerCase().includes(searchText));
+    res.json(matchingProducts);
+  });
 });
