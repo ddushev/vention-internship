5ca37c9e2e1c083bf0368aa8daa3415c4cc69ad8
Daniel Dushev <danieldyshew@gmail.com>
2023-12-06 11:56:12 +0200

    page. hide h2 when its empty

--- src/elements/page/page.tsx
+++ src/elements/page/page.tsx
@@ -8,7 +8,7 @@ interface PageProps {
 export default function Page({ children, title }: PageProps) {
   return (
     <div className={styles.backgroundWrapperImage}>
-      <h2>{title}</h2>
+      {title && <h2>{title}</h2>}
       {children}
     </div>
   );
