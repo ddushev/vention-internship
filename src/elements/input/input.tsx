c8950d577649b56e0d460004f670ca0f78e04a16
Daniel Dushev <danieldyshew@gmail.com>
2023-12-10 19:14:43 +0200

    input. add controlled behaviour

--- src/elements/input/input.tsx
+++ src/elements/input/input.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import styles from "./input.module.scss";
 
 interface InputProps {
@@ -8,10 +9,14 @@ interface InputProps {
 }
 
 export default function Input({ label, type, id, name }: InputProps) {
+  const [value, setValue] = useState("");
+  const onChangeHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setValue(event.target.value);
+  };
   return (
     <div className={styles.inputContainer}>
       <label htmlFor={id}>{label}</label>
-      <input className={styles.input} type={type} id={id} name={name} />
+      <input onChange={(event) => onChangeHandler(event)} className={styles.input} type={type} id={id} name={name} value={value} />
     </div>
   );
 }
