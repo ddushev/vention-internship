81775c497cf65c30bd30be05c4f2ecf0873b39ec
Daniel Dushev <danieldyshew@gmail.com>
2023-12-11 11:37:51 +0200

    input. update to use useForm hook

--- src/elements/input/input.tsx
+++ src/elements/input/input.tsx
@@ -1,4 +1,3 @@
-import { useState } from "react";
 import styles from "./input.module.scss";
 
 interface InputProps {
@@ -6,17 +5,17 @@ interface InputProps {
   type: string;
   id: string;
   name: string;
+  values: {
+    [key: string]: string;
+  };
+  onInputChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
 }
 
-export default function Input({ label, type, id, name }: InputProps) {
-  const [value, setValue] = useState("");
-  const onChangeHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setValue(event.target.value);
-  };
+export default function Input({ label, type, id, name, values, onInputChange }: InputProps) {
   return (
     <div className={styles.inputContainer}>
       <label htmlFor={id}>{label}</label>
-      <input onChange={(event) => onChangeHandler(event)} className={styles.input} type={type} id={id} name={name} value={value} />
+      <input onChange={(event) => onInputChange(event)} className={styles.input} type={type} id={id} name={name} value={values[name]} />
     </div>
   );
 }
