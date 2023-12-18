56aa9aff08cf915818f32a51da58a0ff2579d0fc
Daniel Dushev <danieldyshew@gmail.com>
2023-12-18 17:30:33 +0200

    textarea. add textarea control

---
+++ src/elements/controls/textarea.tsx
@@ -0,0 +1,16 @@
+import { WUPTextareaControl } from "web-ui-pack";
+
+import BaseControl, { BaseControlProps } from "./baseControl";
+
+import styles from "./text.m.scss";
+
+WUPTextareaControl.$use(); // register control in the browser
+// WUPTextareaControl.$defaults.clearButton = false;
+
+interface Props extends BaseControlProps<string, WUPTextareaControl, WUP.Textarea.Options> {}
+
+export default class TextareaControl extends BaseControl<WUPTextareaControl, Props> {
+  goRender(props: Record<string, unknown>): JSX.Element {
+    return <wup-textarea {...props} class={`${styles.ctrl} ${props.className}`.trim()} />;
+  }
+}
