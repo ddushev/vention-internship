38c6a994d1dfc8d52305c30b32cf97e426ba59fb
Daniel Dushev <danieldyshew@gmail.com>
2023-12-19 10:52:49 +0200

    profile. add handleFormSubmit

--- src/components/profile/profile.tsx
+++ src/components/profile/profile.tsx
@@ -6,17 +6,22 @@ import Form from "@/elements/form";
 import TextControl from "@/elements/controls/text";
 import TextareaControl from "@/elements/controls/textarea";
 import NumberControl from "@/elements/controls/number";
+import { WUPFormElement } from "web-ui-pack";
 
 export default function Profile() {
   const authData = useAppSelector((state) => state.authReduxState);
+  const handleFormSubmit = (event: CustomEvent) => {
+    const formData = (event.target as WUPFormElement).$model;
+    console.log(formData);
+  };
   return (
     <Page title="Profile">
       <SectionWrapper heading={`${authData.username} profile page`}>
-        <Form>
-          <TextControl name="Username" validations={{ required: true }} />
-          <TextControl name="Address" validations={{ required: true }} />
-          <NumberControl name="Phone number " validations={{ required: true }} />
-          <TextareaControl name="Profile description" validations={{ required: true }} />
+        <Form onSubmit={handleFormSubmit}>
+          <TextControl name="username" validations={{ required: true }} />
+          <TextControl name="address" validations={{ required: true }} />
+          <NumberControl label="Phone Number" name="phoneNumber" validations={{ required: true }} />
+          <TextareaControl label="Profile Description" name="profileDescription" validations={{ required: true }} />
         </Form>
       </SectionWrapper>
     </Page>
