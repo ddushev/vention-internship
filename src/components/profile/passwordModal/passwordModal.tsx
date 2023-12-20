20679763a4da39ce7b2ffb8f8a86e0365900db6b
Daniel Dushev <danieldyshew@gmail.com>
2023-12-20 13:21:19 +0200

    passwordModal. update validations

--- src/components/profile/passwordModal/passwordModal.tsx
+++ src/components/profile/passwordModal/passwordModal.tsx
@@ -1,4 +1,4 @@
-import { WUPFormElement } from "web-ui-pack";
+import { WUPFormElement, WUPPasswordControl } from "web-ui-pack";
 
 import Modal from "@/elements/controls/wupModal";
 import Form from "@/elements/form/form";
@@ -10,13 +10,7 @@ import styles from "./passwordModal.module.scss";
 export default function PasswordModal({ setIsPassChangeOpen }: { setIsPassChangeOpen: (isClose: boolean) => void }) {
   async function handleFormSubmit(event: CustomEvent) {
     try {
-      const { oldPassword, newPassword, repeatPassword } = (event.target as WUPFormElement).$model;
-      if (oldPassword === newPassword) {
-        throw new Error("New password should be different than the old one!");
-      }
-      if (newPassword !== repeatPassword) {
-        throw new Error("New password should match repeat password!");
-      }
+      const { oldPassword, newPassword } = (event.target as WUPFormElement).$model;
       await changeUserPassword({ oldPassword, newPassword });
     } catch (error) {
       alert(error);
@@ -27,8 +21,17 @@ export default function PasswordModal({ setIsPassChangeOpen }: { setIsPassChange
       <h2 className={styles.modalHeader}>Change password</h2>
       <Form onSubmit={(event) => handleFormSubmit(event)}>
         <PasswordControl name="oldPassword" validations={{ required: true }} />
-        <PasswordControl name="newPassword" isStrict validationShowAll validations={{ required: true }} />
-        <PasswordControl name="repeatPassword" validations={{ required: true }} />
+        <PasswordControl
+          name="newPassword"
+          isStrict
+          validationShowAll
+          validations={{
+            required: true,
+            _old: (v, c) =>
+              v === (c.previousElementSibling as WUPPasswordControl).$value && "New password should be different than the old one!",
+          }}
+        />
+        <PasswordControl name="repeatPassword" validations={{ required: true, confirm: true }} />
         <button className={styles.submitBtn} type="submit">
           Submit
         </button>
