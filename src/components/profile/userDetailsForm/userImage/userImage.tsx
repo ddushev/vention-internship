010d091e6628141b4123a339f69951220f78a9a5
Daniel Dushev <danieldyshew@gmail.com>
2023-12-22 11:31:21 +0200

    userImage. add loading animation

--- src/components/profile/userDetailsForm/userImage/userImage.tsx
+++ src/components/profile/userDetailsForm/userImage/userImage.tsx
@@ -1,4 +1,6 @@
-import { useRef, useState } from "react";
+import loadingAnimation from "images/loading.svg";
+
+import { useEffect, useRef, useState } from "react";
 
 import { updateImage } from "@/api/apiUser";
 import Form from "@/elements/form/form";
@@ -6,8 +8,18 @@ import Button from "@/elements/button/button";
 import styles from "./userImage.module.scss";
 
 export default function UserImage({ profileImg }: { profileImg?: string }) {
+  const [showLoading, setShowLoading] = useState(true);
   const [uploadedImg, setUploadedImg] = useState<string>();
   const fileInputRef = useRef<HTMLInputElement>(null);
+
+  useEffect(() => {
+    const timeoutId = setTimeout(() => {
+      setShowLoading(false);
+    }, 2000);
+
+    return () => clearTimeout(timeoutId);
+  }, []);
+
   const handleUpload = async () => {
     const fileInput = fileInputRef.current;
     if (fileInput && fileInput.files && fileInput.files.length > 0) {
@@ -27,14 +39,22 @@ export default function UserImage({ profileImg }: { profileImg?: string }) {
   return (
     <div className={styles.userImageContainer}>
       <div className={styles.imageSectionContainer}>
-        {uploadedImg && <img className={styles.profileImage} src={uploadedImg} alt="profile-img" />}
-        {!uploadedImg && profileImg && <img className={styles.profileImage} src={profileImg} alt="profile-img" />}
-        {!uploadedImg && !profileImg && (
-          <div className={styles.noImageContainer}>
-            <div className={styles.noImageBox}>
-              <p className={styles.noImageText}>No Image</p>
-            </div>
+        {showLoading ? (
+          <div className={styles.loader}>
+            <img src={loadingAnimation} alt="loading" />
           </div>
+        ) : (
+          <>
+            {uploadedImg && <img className={styles.profileImage} src={uploadedImg} alt="profile-img" />}
+            {!uploadedImg && profileImg && <img className={styles.profileImage} src={profileImg} alt="profile-img" />}
+            {!uploadedImg && !profileImg && (
+              <div className={styles.noImageContainer}>
+                <div className={styles.noImageBox}>
+                  <p className={styles.noImageText}>No Image</p>
+                </div>
+              </div>
+            )}
+          </>
         )}
       </div>
       <Form>
