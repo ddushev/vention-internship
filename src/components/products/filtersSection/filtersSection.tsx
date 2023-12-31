42090112caf2713978126a2232ad05a9b9095d26
Daniel Dushev <danieldyshew@gmail.com>
2023-12-31 12:29:10 +0200

    filterSection & gamesCatalog. add component placeholders

---
+++ src/components/products/filtersSection/filtersSection.tsx
@@ -0,0 +1,5 @@
+import SectionWrapper from "@/elements/sectionWrapper/sectionWrapper";
+
+export default function FiltersSection() {
+  return <SectionWrapper heading="PC">placeholder</SectionWrapper>;
+}
---
+++ src/components/products/gamesCatalog/gamesCatalog.tsx
@@ -0,0 +1,5 @@
+import SectionWrapper from "@/elements/sectionWrapper/sectionWrapper";
+
+export default function GamesCatalog() {
+  return <SectionWrapper heading="Products">placeholder</SectionWrapper>;
+}
