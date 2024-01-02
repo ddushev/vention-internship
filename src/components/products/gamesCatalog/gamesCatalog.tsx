056828b45fbad36f139ec63cb06a2051b4fa0b5c
Daniel Dushev <danieldyshew@gmail.com>
2024-01-02 11:58:55 +0200

    products & gamesCatalog. lift games state in products

--- src/components/products/gamesCatalog/gamesCatalog.tsx
+++ src/components/products/gamesCatalog/gamesCatalog.tsx
@@ -1,19 +1,8 @@
-import { useEffect, useState } from "react";
-import { useLocation } from "react-router-dom";
-
 import GameCard, { Game } from "@/components/gameCard/gameCard";
 
 import SectionWrapper from "@/elements/sectionWrapper/sectionWrapper";
-import getProducts from "@/api/apiProducts";
 
-export default function GamesCatalog() {
-  const [games, setGames] = useState<Game[]>([]);
-  const category = useLocation().search;
-
-  useEffect(() => {
-    getProducts({ category }).then((data) => setGames(data));
-  }, [category]);
-
+export default function GamesCatalog({ games }: { games: Game[] }) {
   return (
     <SectionWrapper heading="Products">
       {games.map((game) => (
--- src/components/products/products.tsx
+++ src/components/products/products.tsx
@@ -1,11 +1,22 @@
+import { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
+
+import getProducts from "@/api/apiProducts";
 import Page from "@/elements/page/page";
 import SearchField from "@/elements/searchField/searchField";
+import { Game } from "@/components/gameCard/gameCard";
 import GamesCatalog from "./gamesCatalog/gamesCatalog";
 import FiltersSection from "./filtersSection/filtersSection";
 
 import styles from "./products.module.scss";
 
 export default function Products() {
+  const [games, setGames] = useState<Game[]>([]);
+  const category = useLocation().search;
+
+  useEffect(() => {
+    getProducts({ category }).then((data) => setGames(data));
+  }, [category]);
   return (
     <Page title="Products">
       <SearchField />
@@ -14,7 +25,7 @@ export default function Products() {
           <FiltersSection />
         </div>
         <div className={styles.catalogContainer}>
-          <GamesCatalog />
+          <GamesCatalog games={games} />
         </div>
       </div>
     </Page>
