4661e92851cf7237828d4c5fde702890a92e9052
Daniel Dushev <danieldyshew@gmail.com>
2024-01-03 11:26:42 +0200

    filterWrapper. add filterWrapper elem with placeholders

--- src/components/products/filtersSection/filtersSection.tsx
+++ src/components/products/filtersSection/filtersSection.tsx
@@ -1,5 +1,12 @@
 import SectionWrapper from "@/elements/sectionWrapper/sectionWrapper";
+import FilterWrapper from "@/elements/filterWrapper/filterWrapper";
 
 export default function FiltersSection() {
-  return <SectionWrapper heading="PC">placeholder</SectionWrapper>;
+  return (
+    <SectionWrapper heading="PC">
+      <FilterWrapper heading="Sort">Sort filter placeholder</FilterWrapper>
+      <FilterWrapper heading="Genre">Genre filter placeholder</FilterWrapper>
+      <FilterWrapper heading="Age">Age filter placeholder</FilterWrapper>
+    </SectionWrapper>
+  );
 }
