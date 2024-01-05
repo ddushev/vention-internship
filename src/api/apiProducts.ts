3f036b54163c3065295d440b82998ec5fb9b7ad5
Daniel Dushev <danieldyshew@gmail.com>
2024-01-05 13:41:18 +0200

    api. import only needed requests.

--- src/api/apiProducts.ts
+++ src/api/apiProducts.ts
@@ -2,7 +2,7 @@ import apiEndpoints from "@/api.endpoints";
 
 import handleErrors from "@/utils/handleErrors";
 import { Game } from "@/components/gameCard/gameCard";
-import * as api from "./requests";
+import { get } from "./requests";
 
 interface GetProductsParams {
   urlParams: string;
@@ -10,7 +10,7 @@ interface GetProductsParams {
 
 export default async function getProducts({ urlParams }: GetProductsParams): Promise<Game[]> {
   try {
-    const data = await api.get(`${apiEndpoints.getProducts}?${urlParams}`);
+    const data = await get(`${apiEndpoints.getProducts}?${urlParams}`);
     return data as Game[];
   } catch (error) {
     handleErrors(error);
--- src/api/apiUser.ts
+++ src/api/apiUser.ts
@@ -4,16 +4,16 @@ import { dispatch } from "@/redux/store";
 import { setAuthState } from "@/redux/authSlice";
 import handleErrors from "@/utils/handleErrors";
 
-import * as api from "./requests";
+import { get, update } from "./requests";
 
 export async function getUserProfile() {
-  const userData = await api.get(apiEndpoints.getProfile);
+  const userData = await get(apiEndpoints.getProfile);
   return userData;
 }
 
 export async function updateUserProfile({ username, address, phone, description }: UserMockData) {
   try {
-    const updatedUserName = await api.update(apiEndpoints.saveProfile, { username, address, phone, description });
+    const updatedUserName = await update(apiEndpoints.saveProfile, { username, address, phone, description });
     dispatch(setAuthState(updatedUserName));
   } catch (error) {
     handleErrors(error);
@@ -22,7 +22,7 @@ export async function updateUserProfile({ username, address, phone, description 
 
 export async function changeUserPassword({ oldPassword, newPassword }: { oldPassword: string; newPassword: string }) {
   try {
-    await api.update(apiEndpoints.changePassword, { oldPassword, newPassword });
+    await update(apiEndpoints.changePassword, { oldPassword, newPassword });
   } catch (error) {
     handleErrors(error);
   }
