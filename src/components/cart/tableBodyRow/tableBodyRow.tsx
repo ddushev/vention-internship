97894150c35a7962e2a28daa6e441f3fbe4f319b
Daniel Dushev <danieldyshew@gmail.com>
2024-01-10 11:59:47 +0200

    tableBodyRow. add MUI NumberInput

--- src/components/cart/tableBodyRow/tableBodyRow.tsx
+++ src/components/cart/tableBodyRow/tableBodyRow.tsx
@@ -1,4 +1,5 @@
 import { MenuItem, Select, SelectChangeEvent } from "@mui/material";
+import { Unstable_NumberInput as NumberInput } from "@mui/base/Unstable_NumberInput";
 
 import TableData from "@/elements/tableData/tableData";
 import TableRow from "@/elements/tableRow/tableRow";
@@ -14,8 +15,10 @@ const selectStyles = {
   backgroundColor: "rgba(0, 0, 0, 0.5)",
   border: "1px solid white",
   textAlign: "left",
+  borderRadius: "0",
   "& .MuiSelect-select": { padding: "10px" },
   "& .MuiOutlinedInput-notchedOutline": { border: "none" },
+  "& .MuiSelect-icon": { color: "white" },
 };
 
 const menuProps = {
@@ -30,10 +33,39 @@ const menuProps = {
   },
 };
 
+const numberInputSlotProps = {
+  input: {
+    style: {
+      maxWidth: "100px",
+      padding: "10px",
+      border: "1px solid white",
+      color: "white",
+      backgroundColor: "rgba(0, 0, 0, 0.5)",
+    },
+  },
+  decrementButton: {
+    style: {
+      display: "none",
+    },
+  },
+  incrementButton: {
+    style: {
+      display: "none",
+    },
+  },
+};
+
 export default function TableBodyRow({ game }: { game: Game }) {
   const [platform, setPlatform] = useState(game.platforms[0]);
+  const [amount, setAmount] = useState(1);
   const onSelectChange = (event: SelectChangeEvent) => {
     setPlatform(event.target.value);
+  };
+  const onNumberInputChange = (
+    _event: React.FocusEvent<HTMLInputElement, Element> | React.PointerEvent<Element> | React.KeyboardEvent<Element>,
+    value: number | undefined,
+  ) => {
+    setAmount(value || 1);
   };
   const currentDate = createDate();
   return (
@@ -49,7 +81,9 @@ export default function TableBodyRow({ game }: { game: Game }) {
         </Select>
       </TableData>
       <TableData>{currentDate}</TableData>
-      <TableData>1</TableData>
+      <TableData>
+        <NumberInput onChange={onNumberInputChange} value={amount} slotProps={numberInputSlotProps} />
+      </TableData>
       <TableData>{game.price}</TableData>
     </TableRow>
   );
