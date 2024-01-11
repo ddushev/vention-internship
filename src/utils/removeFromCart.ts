75ed57d5045554954218d6265a776fbd4f65ffef
Daniel Dushev <danieldyshew@gmail.com>
2024-01-11 11:59:46 +0200

    removeFromCart. update removeFromLocal to update redux state

--- src/utils/removeFromCart.ts
+++ src/utils/removeFromCart.ts
@@ -1,13 +1,16 @@
+import { dispatch } from "@/redux/store";
+import { setCartState } from "@/redux/cartSlice";
 import { Game } from "@/types";
 
-const removeFromLocalStorage = (key: string, gamesInStateCart: Game[]) => {
+const removeFromCart = (key: string, gamesInStateCart: Game[]) => {
   const cartInStorage = localStorage.getItem(key);
 
   if (cartInStorage) {
     const parsedCart = JSON.parse(cartInStorage);
     const updatedCart = parsedCart.filter((g: Game) => !gamesInStateCart.some((gameInStateCart: Game) => gameInStateCart.name === g.name));
+    dispatch(setCartState(updatedCart));
     localStorage.setItem(key, JSON.stringify(updatedCart));
   }
 };
 
-export default removeFromLocalStorage;
+export default removeFromCart;
