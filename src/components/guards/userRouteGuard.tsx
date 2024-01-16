2251a8338ba5091edbcaec1bb690a3d46682f783
Daniel Dushev <danieldyshew@gmail.com>
2024-01-16 10:41:16 +0200

    userRouteGuard. fix issue with modal opening and closing when we have session

--- src/components/guards/userRouteGuard.tsx
+++ src/components/guards/userRouteGuard.tsx
@@ -1,20 +1,32 @@
 import { useEffect, useState } from "react";
 import { Outlet } from "react-router-dom";
 
-import { useAppSelector } from "@/redux/hooks";
+import { useAppDispatch, useAppSelector } from "@/redux/hooks";
+import { setAuthState } from "@/redux/authSlice";
+import { getUserProfile } from "@/api/apiUser";
+import { AuthData, UserMockData } from "@/types";
 
 import SignInModal from "@/components/account/signInModal";
 
 export default function UserRouteGuard() {
+  const dispatch = useAppDispatch();
+  const dispatchSetAuthState = (data: AuthData) => {
+    dispatch(setAuthState(data));
+  };
   const [isSignInOpen, setIsSignInOpen] = useState(false);
   const [isSession, setIsSession] = useState(true);
   const authData = useAppSelector((state) => state.authReduxState);
+
   useEffect(() => {
-    if (authData?.username || isSession) {
+    if (!authData.username && isSession) {
+      getUserProfile().then((userData: UserMockData) => dispatchSetAuthState({ username: userData.username }));
       setIsSession(false);
-      setIsSignInOpen(false);
-    } else {
+    }
+
+    if (!authData?.username && !isSession) {
       setIsSignInOpen(true);
+    } else {
+      setIsSignInOpen(false);
     }
   }, [authData]);
 
