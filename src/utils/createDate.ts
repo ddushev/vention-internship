66df557f0846291035f78d52435f7f0b4751a3e0
Daniel Dushev <danieldyshew@gmail.com>
2024-01-16 21:56:39 +0200

    createDate. add separator argument

--- src/utils/createDate.ts
+++ src/utils/createDate.ts
@@ -1,11 +1,11 @@
-const createDate = () => {
+const createDate = (separator: string = "-") => {
   const date = new Date();
 
   const year = date.getFullYear();
   const month = String(date.getMonth() + 1).padStart(2, "0");
   const day = String(date.getDate()).padStart(2, "0");
 
-  const formattedDate = `${month}/${day}/${year}`;
+  const formattedDate = `${month}${separator}${day}${separator}${year}`;
 
   return formattedDate;
 };
