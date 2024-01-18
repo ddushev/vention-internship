b6b4cd1bbf9510f7b2161f7aa680a256a5e27cd5
Daniel Dushev <danieldyshew@gmail.com>
2024-01-18 10:34:20 +0200

    editCardModal. use delCardModal when user clicks delete button

--- src/components/gameCard/editCardModal/editCardModal.tsx
+++ src/components/gameCard/editCardModal/editCardModal.tsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 import { updateProduct } from "@/api/apiProducts";
 import { setProductState } from "@/redux/productSlice";
 import { useAppDispatch } from "@/redux/hooks";
@@ -5,14 +7,18 @@ import updateGameInCart from "@/utils/updateGameInCart";
 
 import Modal from "@/elements/controls/wupModal";
 import Button from "@/elements/button/button";
-
-import { Game, Product } from "@/types";
 import GameForm from "@/elements/gameForm/gameForm";
+import { Game, Product } from "@/types";
+import DeleteCardModal from "../deleteCardModal/deleteCardModal";
+
 import styles from "./editCardModal.module.scss";
 
 export default function EditCardModal({ setIsCardModalOpen, game }: { setIsCardModalOpen: (isClose: boolean) => void; game: Game }) {
   const dispatch = useAppDispatch();
-
+  const [isDeleteCardModalOpen, setIsDeleteCardModalOpen] = useState(false);
+  const handleDeleteCardClick = () => {
+    setIsDeleteCardModalOpen(true);
+  };
   const onSubmitHandler = async (data: Product) => {
     const games: Game[] = await updateProduct(data, game.id);
     dispatch(setProductState(games));
@@ -20,24 +26,27 @@ export default function EditCardModal({ setIsCardModalOpen, game }: { setIsCardM
     updateGameInCart(updatedGame);
   };
   return (
-    <Modal onClose={() => setIsCardModalOpen(false)} className={styles.modalContainer}>
-      <h2 className={styles.modalHeader}>Create Card</h2>
-      <div className={styles.imgFormContainer}>
-        <div className={styles.imgContainer}>
-          <h3 className={styles.subheadings}>Card image</h3>
-          <img className={styles.img} src={game.image} alt={`${game.name}-img`} />
-        </div>
-        <GameForm game={game} onSubmitHandler={onSubmitHandler}>
-          <div className={styles.heightContainer}>
-            <div className={styles.buttonContainer}>
-              <Button className={styles.addGameButton} submit>
-                Submit
-              </Button>
-              <Button>Delete card</Button>
-            </div>
+    <>
+      <Modal onClose={() => setIsCardModalOpen(false)} className={styles.modalContainer}>
+        <h2 className={styles.modalHeader}>Create Card</h2>
+        <div className={styles.imgFormContainer}>
+          <div className={styles.imgContainer}>
+            <h3 className={styles.subheadings}>Card image</h3>
+            <img className={styles.img} src={game.image} alt={`${game.name}-img`} />
           </div>
-        </GameForm>
-      </div>
-    </Modal>
+          <GameForm game={game} onSubmitHandler={onSubmitHandler}>
+            <div className={styles.heightContainer}>
+              <div className={styles.buttonContainer}>
+                <Button className={styles.addGameButton} submit>
+                  Submit
+                </Button>
+                <Button onClick={handleDeleteCardClick}>Delete card</Button>
+              </div>
+            </div>
+          </GameForm>
+        </div>
+      </Modal>
+      {isDeleteCardModalOpen && <DeleteCardModal gameName={game.name} id={game.id} setIsDeleteCardModalOpen={setIsDeleteCardModalOpen} />}
+    </>
   );
 }
