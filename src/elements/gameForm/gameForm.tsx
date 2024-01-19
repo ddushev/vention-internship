d7209ef6453b03d283e5ea97d9e67e2a92f7cc91
Daniel Dushev <danieldyshew@gmail.com>
2024-01-19 14:49:53 +0200

    gameForm. use form initModel instead of control initValue

--- src/elements/gameForm/gameForm.tsx
+++ src/elements/gameForm/gameForm.tsx
@@ -19,34 +19,37 @@ interface GameFormProps {
 }
 
 export default function GameForm({ children, game, onSubmitHandler }: GameFormProps) {
+  const initModel = {
+    ...game,
+    category: game?.genre,
+    minAge: game?.minAge.toString(),
+    pc: game?.platforms.includes("pc"),
+    ps5: game?.platforms.includes("ps5"),
+    xbox: game?.platforms.includes("xbox"),
+  };
   return (
-    <Form className={styles.form} onSubmit={onSubmitHandler}>
+    <Form initModel={initModel} className={styles.form} onSubmit={onSubmitHandler}>
       <h3 className={styles.subheadings}>Information</h3>
       <ControlWrapper text="Name">
-        <TextControl initValue={game?.name} className={styles.controls} name="name" validations={{ required: true }} />
+        <TextControl className={styles.controls} name="name" validations={{ required: true }} />
       </ControlWrapper>
       <ControlWrapper text="Category">
-        <TextControl initValue={game?.genre} className={styles.controls} name="category" validations={{ required: true }} />
+        <TextControl className={styles.controls} name="category" validations={{ required: true }} />
       </ControlWrapper>
       <ControlWrapper text="Price">
-        <NumberControl initValue={game?.price} className={styles.controls} name="price" validations={{ required: true }} />
+        <NumberControl className={styles.controls} name="price" validations={{ required: true }} />
       </ControlWrapper>
       <ControlWrapper text="Image">
-        <TextControl initValue={game?.image} className={styles.controls} name="image" validations={{ required: true }} />
+        <TextControl className={styles.controls} name="image" validations={{ required: true }} />
       </ControlWrapper>
       <ControlWrapper text="Description">
-        <TextareaControl
-          initValue={game?.description}
-          className={cx(styles.controls, styles.descriptionControl)}
-          name="description"
-          validations={{ required: true }}
-        />
+        <TextareaControl className={cx(styles.controls, styles.descriptionControl)} name="description" validations={{ required: true }} />
       </ControlWrapper>
       <ControlWrapper text="Age">
         <SelectControl
           className={cx(styles.controls, styles.ageControl)}
           name="minAge"
-          initValue={game?.minAge.toString() || "3"}
+          initValue="3"
           items={[
             { text: "3+", value: "3" },
             { text: "6+", value: "6" },
@@ -58,13 +61,13 @@ export default function GameForm({ children, game, onSubmitHandler }: GameFormPr
 
       <h3 className={styles.subheadings}>Platform</h3>
       <ControlWrapper text="PC">
-        <CheckControl initValue={game?.platforms.includes("pc")} className={cx(styles.controls, styles.checkControl)} name="pc" />
+        <CheckControl className={cx(styles.controls, styles.checkControl)} name="pc" />
       </ControlWrapper>
       <ControlWrapper text="PlayStation 5">
-        <CheckControl initValue={game?.platforms.includes("ps5")} className={cx(styles.controls, styles.checkControl)} name="ps5" />
+        <CheckControl className={cx(styles.controls, styles.checkControl)} name="ps5" />
       </ControlWrapper>
       <ControlWrapper text="XBox One">
-        <CheckControl initValue={game?.platforms.includes("xbox")} className={cx(styles.controls, styles.checkControl)} name="xbox" />
+        <CheckControl className={cx(styles.controls, styles.checkControl)} name="xbox" />
       </ControlWrapper>
       {children}
     </Form>
