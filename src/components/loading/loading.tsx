53fb7667994392ae5243e1822b55a44996840153
Daniel Dushev <danieldyshew@gmail.com>
2024-03-05 15:13:31 +0200

    loading. memoize component

--- src/components/loading/loading.tsx
+++ src/components/loading/loading.tsx
@@ -1,12 +1,14 @@
+import { memo } from "react";
+
 import cx from "classnames";
 import loading from "images/loading.svg";
 
 import styles from "./loading.module.scss";
 
-export default function Loading({ isLazyLoading }: { isLazyLoading?: boolean | undefined }) {
-  return (
-    <div className={cx({ [styles.lazyLoading]: isLazyLoading })}>
-      <img src={loading} alt="loading-img" />
-    </div>
-  );
-}
+const Loading = memo(({ isLazyLoading }: { isLazyLoading?: boolean | undefined }) => (
+  <div className={cx({ [styles.lazyLoading]: isLazyLoading })}>
+    <img src={loading} alt="loading-img" />
+  </div>
+));
+
+export default Loading;
